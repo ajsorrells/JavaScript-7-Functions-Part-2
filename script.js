3afd2797cca2804359c90f08c548765750b116bd
ajsorrells <167811075+ajsorrells@users.noreply.github.com>
2025-03-31 09:26:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function changeGreeting(){
+    document.getElementById("greeting").textContent = `Hello, JavaScript!`;
+}
+
+function changeBackground(){
+    document.body.style.backgroundColor = `lightblue`;
+}
+
+function showName(){
+    let name = document.getElementById("userInput").value;
+    document.getElementById("Output").textContent = `Hello, ${name}!`;
+}
+
+let count = 0;
+function increasedCount(){
+    count++;
+    document.getElementById("count").textContent = `${count}`;
+}
+
+function changeImage(){
+    document.getElementById("myImage").src=`image2.jpg`;
+}
+
+function toggleText(){
+    let text = document.getElementById("text");
+    text.style.display = text.style.display === "none"?"block":"none";
+}
+
+function increaseFontSize(){
+    document.getElementById("textSize").style.fontSize = `24px`;
+}
+
+function updateContent(){
+    document.getElementById("title").textContent=`New Title`;
+    document.getElementById("description").textContent=`This text has changed!`;
+    document.getElementById("description").style.color=`red`;
+}
+
+function calculateSquare(){
+    let num = document.getElementById("numberInput").value;
+    let square = num * num;
+    document.getElementById("result").textContent = `The square of ${num} is ${square}`;
+}
+
